4d424f4a86df1fa73995cf1f48b268265969d212
Stefano Formicola <stefanoformicola@icloud.com>
2025-01-20 23:46:09 +0100

    refactor: move app files into src/

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: '.next',
+  reactStrictMode: true,
+}
 
-export default nextConfig;
+export default nextConfig
